d60e9e6aea734c53e03413ddd8ab5dfe40009267
thomas raphel <thomasrafel@gmail.com>
2018-10-15 15:11:09 -0400

    ARCHITECTURE as of now app component has for loop for serverelements server element just shows the details we should not store server elements in app comp, will see how to communicate between components next

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
