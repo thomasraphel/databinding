4c8d4632705fb9d733b69e3516c23a7c5c15dc22
thomas raphel <thomasrafel@gmail.com>
2018-10-15 17:07:38 -0400

    added an event on cockpit so that when server is added, app component is notified of it

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -11,6 +11,20 @@ export class AppComponent {
     {type : 'server', name : 'TestSErver', content : 'Just a test!!'}
   ];
   
+  onServerAdded(serverData : {serverName : string, content : string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.content
+    });
+  }
 
+  onBlueprintAdded(blueprintData : {serverName : string, content : string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.content
+    });
+  }
   
 }
