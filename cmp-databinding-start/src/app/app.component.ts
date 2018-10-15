48ded834e79a25067d7780ee16a2d5b44faf8b0f
thomas raphel <thomasrafel@gmail.com>
2018-10-15 14:20:38 -0400

    put the logic and variables needed for server creattion to cockpit and server details display in server -element, also took out the ngFor from the server element html

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -7,8 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [];
-  newServerName = '';
-  newServerContent = '';
+  
 
   
 }
--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
 
+  newServerName = '';
+  newServerContent = '';
+
   onAddServer() {
     this.serverElements.push({
       type: 'server',
