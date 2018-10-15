14902953504b93833c80c32f9c551340c1ac02bc
thomas raphel <thomasrafel@gmail.com>
2018-10-15 16:43:10 -0400

    all properties inside a component is only for use or visibility inside the component import input decorator from angular core and use it on the property you want to bind with in the parent component app comp, youo canbind to the property of the server element using the server element selector,,now you can bind to the element property from outside world input is used because this property gets inpput or value from the outside world

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
   
+  @Input()
   element : {type : string, name : string, content : string};
   //kind of property or the data type,, just line ' element : Ingredient '
 
